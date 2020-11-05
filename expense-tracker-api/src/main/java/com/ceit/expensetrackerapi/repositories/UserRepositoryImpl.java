c6462c3019e28ffb19120156252f69ffa28ce45c
JishnuTU <jishnutu1995@gmail.com>
2020-11-05 14:41:21 +1000

    created function findById and RowMapper for USer Object

--- expense-tracker-api/src/main/java/com/ceit/expensetrackerapi/repositories/UserRepositoryImpl.java
+++ expense-tracker-api/src/main/java/com/ceit/expensetrackerapi/repositories/UserRepositoryImpl.java
@@ -2,10 +2,20 @@ package com.ceit.expensetrackerapi.repositories;
 
 import com.ceit.expensetrackerapi.domains.User;
 import com.ceit.expensetrackerapi.exceptions.EAuthException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class UserRepositoryImpl implements UserRepository {
+
+    private static final String SQL_FIND_BY_ID = "SELECT user_id, first_name," +
+            "last_name,email,password FROM et_users WHERE user_id = ?";
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Override
     public Integer create(String firstName, String lastName, String email, String password) throws EAuthException {
         return null;
@@ -23,6 +33,19 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public User findById(Integer userId) {
-        return null;
+        return jdbcTemplate.queryForObject(SQL_FIND_BY_ID,new Object[]{userId},
+                userRowMapper);
+
     }
+
+    private RowMapper<User> userRowMapper = ((rs,rowNum)->{
+        return new User (
+                rs.getInt("USER_ID"),
+                rs.getString("FIRST_NAME"),
+                rs.getString("LASTNAME"),
+                rs.getString("EMAIL"),
+                rs.getString("PASSWORD")
+                );
+    });
+
 }
