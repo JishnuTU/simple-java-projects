ad57768cd11f91e1f4779890a6e151dd0bb555c9
JishnuTU <jishnutu1995@gmail.com>
2020-11-05 15:22:04 +1000

    User Create function

--- expense-tracker-api/src/main/java/com/ceit/expensetrackerapi/repositories/UserRepositoryImpl.java
+++ expense-tracker-api/src/main/java/com/ceit/expensetrackerapi/repositories/UserRepositoryImpl.java
@@ -5,7 +5,12 @@ import com.ceit.expensetrackerapi.exceptions.EAuthException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 
 @Repository
 public class UserRepositoryImpl implements UserRepository {
@@ -21,12 +26,30 @@ public class UserRepositoryImpl implements UserRepository {
 
     private static final String SQL_CREATE = "INSERT INTO ET_USERS(USER_ID,FIRST_NAME," +
             "LAST_NAME,EMAIL,PASSWORD VALUES (NEXTVAL('ET_USERS_SEQ'),?,?,?,?)";
+
     @Autowired
     JdbcTemplate jdbcTemplate;
 
     @Override
     public Integer create(String firstName, String lastName, String email, String password) throws EAuthException {
-        return null;
+        try{
+            KeyHolder keyHolder = new GeneratedKeyHolder();
+            jdbcTemplate.update(connection -> {
+                PreparedStatement ps = connection.prepareStatement(SQL_CREATE,
+                        Statement.RETURN_GENERATED_KEYS);
+                ps.setString(1,firstName);
+                ps.setString(2,lastName);
+                ps.setString(3,email);
+                ps.setString(4,password);
+                return ps;
+            },keyHolder);
+
+            return (Integer) keyHolder.getKeys().get("USER_ID");
+            
+        }
+        catch (Exception e){
+         throw new EAuthException("Invalid Details , Failed to Create an Acoount");
+        }
     }
 
     @Override
