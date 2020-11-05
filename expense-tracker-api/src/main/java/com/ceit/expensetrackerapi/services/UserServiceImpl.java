fec0273f59d39e8b61a8f78f86ee8a12909abcfc
lab1pc <lab1pc@localhost.localdomain>
2020-11-05 14:22:30 +1000

    created  UserService:registerUser function to handle register

---
+++ expense-tracker-api/src/main/java/com/ceit/expensetrackerapi/services/UserServiceImpl.java
@@ -0,0 +1,40 @@
+package com.ceit.expensetrackerapi.services;
+
+import com.ceit.expensetrackerapi.domains.User;
+import com.ceit.expensetrackerapi.exceptions.EAuthException;
+import com.ceit.expensetrackerapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.regex.Pattern;
+
+public class UserServiceImpl implements  UserService{
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public User validateUser(String email, String password) throws EAuthException {
+        return null;
+    }
+
+    @Override
+    public User registerUser(String firstName, String lastName, String email, String password) throws EAuthException {
+        Pattern pattern = Pattern.compile("^(.+)@(.+)$");
+        if(email!= null)
+            email = email.toLowerCase();
+        // Checking the email is in correct pattern
+        if(!pattern.matcher(email).matches())
+            throw new EAuthException("Invalid Email Format");
+        // Checking email already exists
+        Integer count = userRepository.getCountByEmail(email);
+
+        if(count>0)
+            throw new EAuthException("Email Already in Use");
+
+        // We can register the user using the create api
+
+        Integer userId = userRepository.create(firstName,lastName,email,password);
+        // Return the User object from the database;
+        return userRepository.findById(userId);
+    }
+}
