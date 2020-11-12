2c4a7c3422604c64c4820d76d5bc2e80f8cdbfee
JishnuTU <jishnutu1995@gmail.com>
2020-11-12 14:22:16 +1000

    interface for category repo

---
+++ expense-tracker-api/src/main/java/com/ceit/expensetrackerapi/repositories/CategoryRepository.java
@@ -0,0 +1,24 @@
+package com.ceit.expensetrackerapi.repositories;
+
+import com.ceit.expensetrackerapi.domains.Category;
+
+import java.util.List;
+
+public interface CategoryRepository {
+
+    Integer create (Integer userId, String title,
+                    String description)
+            throws EtBadRequestException;
+
+    void update(Integer userId, Integer categoryId,
+                Category category) throws EtBadRequestException;
+
+    void removeById(Integer userId, Integer categoryId);
+
+    Category findById(Integer userId, Integer categoryId)
+            throws EtResourceNotFoundException;
+    
+    List<Category> findAll(Integer userId)
+        throws EtResourceNotFoundException;
+
+}
