577fb020766d85c52f8b376d0a40de6cc83b88c1
JishnuTU <jishnutu1995@gmail.com>
2020-11-17 14:49:47 +1000

    transaction compoents

---
+++ expense-tracker-api/src/main/java/com/ceit/expensetrackerapi/repositories/TransactionRepository.java
@@ -0,0 +1,24 @@
+package com.ceit.expensetrackerapi.repositories;
+
+import com.ceit.expensetrackerapi.domains.Transaction;
+import com.ceit.expensetrackerapi.exceptions.EtBadRequestException;
+import com.ceit.expensetrackerapi.exceptions.EtResourceNotFoundException;
+
+import java.util.List;
+
+public interface TransactionRepository {
+
+    List<Transaction> findAll(Integer userId, Integer categoryId);
+
+    Transaction findById(Integer userId, Integer categoryId, Integer transactionId)
+        throws EtResourceNotFoundException;
+
+    Integer create(Integer userId,Integer categoryId, Double amount, String note,
+                   Long transactionDate) throws EtBadRequestException;
+
+    void update(Integer userId, Integer categoryId, Integer transactionId,
+                Transaction transaction) throws  EtBadRequestException;
+
+    void removeById(Integer userId, Integer categoryId, Integer trasnactionId)
+            throws EtResourceNotFoundException;
+}
